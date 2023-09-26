7c143e026668a4fba2ef418dd78a52ccc5954543
abayomi <abayominice@gmail.com>
2023-09-26 17:55:01 +0100

    commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
+const path = require('path');
 require("dotenv").config({ path: path.resolve(__dirname, '/.env') });
 const express = require('express');
-const path = require('path');
+
 const app = express();
 const port = 3000;
 const Flutterwave = require('flutterwave-node-v3');
