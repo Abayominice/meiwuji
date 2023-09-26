70a49d49f94d21d621c907eb4d8e101f6a713cef
abayomi <abayominice@gmail.com>
2023-09-26 17:47:28 +0100

    commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Flutterwave = require('flutterwave-node-v3');
 
 const flw = new Flutterwave(process.env.PUBLIC_KEY, process.env.SECRET_KEY);
 console.log("PUBLIC_KEY:", process.env.PUBLIC_KEY);
-console.log("Env File Path:", path.resolve(__dirname, '.env'));
+console.log("Env File Path:", path.resolve(__dirname, '/.env'));
 
 app.use(express.urlencoded({extended: true}));
 
