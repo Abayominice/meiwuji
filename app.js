0121232e306b88c0d1818368da6fb230696a6827
Aaron Adetunmbi <adetunmbikenny@gmail.com>
2023-10-01 10:02:34 +0100

    .

--- app.js
+++ app.js
@@ -3,100 +3,84 @@ require = require("esm")(module);
 
 // Now you can use import syntax in your CommonJS file
 
-
-
 require("dotenv").config();
-const express = require ('express');
-const { default: got } = import('got');
-process.env.PUBLIC_KEY = 'FLWPUBK_TEST-939acb7e72fe1cc610ea491135e2e4b1-X';
-process.env.SECRET_KEY = 'FLWSECK_TEST-c5d6862c3d99722f93fba9acee6371e1-X';
-process.env.ENCRYPTION_KEY = 'FLWSECK_TESTdae0705e3852';
+const express = require("express");
+const { default: got } = import("got");
+process.env.PUBLIC_KEY = "FLWPUBK_TEST-939acb7e72fe1cc610ea491135e2e4b1-X";
+process.env.SECRET_KEY = "FLWSECK_TEST-c5d6862c3d99722f93fba9acee6371e1-X";
+process.env.ENCRYPTION_KEY = "FLWSECK_TESTdae0705e3852";
 
 const app = express();
 
 const port = 3000;
 
-const Flutterwave = require('flutterwave-node-v3');
-
-const flw = new Flutterwave(process.env.PUBLIC_KEY, process.env.SECRET_KEY);
-
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 
 // Routes for specific pages
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/public/index.html");
-  });
-  
-  app.get("/books", (req, res) => {
-	res.sendFile(__dirname + "/public/books.html");
-  });
-  app.get("/checkout", (req, res) => {
-	res.sendFile(__dirname + "/public/checkout.html");
-  });
-  app.get("/conseqshppg", (req, res) => {
-	res.sendFile(__dirname + "/public/conseqshppg.html");
-  });
-  app.get("/download", (req, res) => {
-	res.sendFile(__dirname + "/public/download.html");
-  });
-  app.get("/preview", (req, res) => {
-	res.sendFile(__dirname + "/public/preview.html");
-  });
-  app.get("/shop", (req, res) => {
-	res.sendFile(__dirname + "/public/shop.html");
-  });
-  app.get("/cart", (req, res) => {
-    res.sendFile(__dirname + "/public/cart.html");
-    });
+  res.sendFile(__dirname + "/public/index.html");
+});
 
+app.get("/books", (req, res) => {
+  res.sendFile(__dirname + "/public/books.html");
+});
+app.get("/checkout", (req, res) => {
+  res.sendFile(__dirname + "/public/checkout.html");
+});
+app.get("/conseqshppg", (req, res) => {
+  res.sendFile(__dirname + "/public/conseqshppg.html");
+});
+app.get("/download", (req, res) => {
+  res.sendFile(__dirname + "/public/download.html");
+});
+app.get("/preview", (req, res) => {
+  res.sendFile(__dirname + "/public/preview.html");
+});
+app.get("/shop", (req, res) => {
+  res.sendFile(__dirname + "/public/shop.html");
+});
+app.get("/cart", (req, res) => {
+  res.sendFile(__dirname + "/public/cart.html");
+});
 
-
-  
 // Define a route for processing payments
-app.post('/processPayment', async (req, res) => {
+app.get("/processPayment/:txRef", async (req, res) => {
   // Retrieve payment data from the request body
-  const paymentData = req.body;
-  
-  const fetch = await import('node-fetch');
+  const txRef = req.params.txRef;
+
+  const fetch = await import("node-fetch");
 
   // Handle payment processing using got or any other library you prefer
   try {
-    const response = await fetch.default('https://api.flutterwave.com/v3/payments', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${process.env.SECRET_KEY}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        ...paymentData, // Include payment data received from the client
-        redirect_url: 'https://www.meiwuji.store/download', 
-        customizations: {
-          title: 'CONSEQUENCES',
-          logo: 'https://drive.google.com/file/d/1824LP_tZrHTjMmcINYJ9gB-F4YAhcTxQ/view?usp=sharing',
+    const response = await fetch.default(
+      `https://api.flutterwave.com/v3/transactions/${txRef}/verify`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.SECRET_KEY}`,
+          "Content-Type": "application/json",
         },
-      }),
-    });
+      }
+    );
 
     const result = await response.json();
-    
+    console.log(result);
+    if (result.status == "error") {
+      return res.status(400).json({
+        success: false,
+        message: "No transaction was found for this id",
+      });
+    }
 
-    // Handle the response here and send it back to the client
     res.json(result);
   } catch (error) {
     console.error(error);
     // Handle errors as needed and send an appropriate response
-    res.status(500).json({ success: false, message: 'Payment processing error' });
+    res
+      .status(500)
+      .json({ success: false, message: "Payment processing error" });
   }
 });
-
-
-
-
-
-
-  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,33 +1,115 @@
-import { initializePayment } from '/scripts/cart.js'; 
-console.log(initializePayment);
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
-function myFunction() { 
-    const menu_item = document.querySelector(".menuitems");
-    const menu_toggler = document.querySelector(".icimg");
-    menu_item.classList.toggle("show");
-  }
-  
-  function hideNav() {
-    const main_menu_is_visible = document.querySelector(".show");
-    if (main_menu_is_visible) {
-      const menu_item = document.querySelector(".menuitems");
-      menu_item.classList.remove("show");
+// function myFunction() {
+//   const menu_item = document.querySelector(".menuitems");
+//   const menu_toggler = document.querySelector(".icimg");
+//   menu_item.classList.toggle("show");
+// }
+
+// function hideNav() {
+//   const main_menu_is_visible = document.querySelector(".show");
+//   if (main_menu_is_visible) {
+//     const menu_item = document.querySelector(".menuitems");
+//     menu_item.classList.remove("show");
+//   }
+// }
+
+// Get a reference to the pay button by its ID (assuming the button has an ID of "payButton")
+// const payButton = document.getElementById("payButton");
+
+// // Add a click event listener to the pay button
+// payButton.addEventListener("click", async () => {
+//   // Call the payNow function when the button is clicked
+//   await initializePayment();
+// });
+
+const emailForm = document.getElementById("payButton");
+
+emailForm &&
+  emailForm.addEventListener(
+    "click",
+    async (event) => {
+      event.preventDefault(); // Prevent default form submission behavior
+
+      const email = document.querySelector(".email-input").value;
+      const name = document.querySelector(".name-input").value;
+      const phone = document.querySelector(".phone-input").value;
+
+      const submitButton = document.querySelector(".email-form-submit");
+
+      const data = {
+        email,
+        name,
+        phone,
+      };
+
+      var publicKey = "FLWPUBK_TEST-939acb7e72fe1cc610ea491135e2e4b1-X";
+      var txRef = Date.now().toString();
+      var amount = 1500; // Amount in the smallest currency unit (e.g., kobo in NGN)
+      var currency = "NGN"; // Currency code
+
+      try {
+        const onPaymentDone = async (submitButton) => {
+          submitButton.textContent = "loading...";
+
+          submitButton.textContent = "Successfull";
+          submitButton.style.opacity = "0.5";
+          submitButton.disabled = true;
+        };
+
+        var paymentData = {
+          tx_ref: txRef,
+          amount: amount,
+          currency: currency,
+          // redirect_url: "https://yourwebsite.com/download", // Redirect URL after payment
+          payment_type: "card", // Payment type (card, bank_transfer, etc.)
+          customer: {
+            email,
+            name,
+            phone,
+          },
+          meta: {
+            email,
+            name,
+            phone,
+          },
+          public_key: publicKey,
+          callback: async (data) => {
+            await verifyPayment(data.transaction_id);
+            await onPaymentDone(submitButton);
+          },
+        };
+        FlutterwaveCheckout(paymentData);
+      } catch (err) {
+        submitButton.textContent = "Error. Try again later";
+
+        submitButton.disabled = true;
+        submitButton.style.opacity = "0.5";
+      }
+    },
+    true
+  );
+
+const verifyPayment = async (txRef) => {
+  try {
+    const response = await fetch(`/processPayment/${txRef}`, {
+      method: "GET",
+    });
+
+    const result = await response.json();
+    if (result.status == "success") {
+      location.assign("/download");
     }
+  } catch (error) {
+    // location.assign("/error");
   }
+};
 
+const onPaymentDone = async (submitButton) => {
+  submitButton.textContent = "loading...";
 
-  
-
-
-
-  
-  // Get a reference to the pay button by its ID (assuming the button has an ID of "payButton")
-  const payButton = document.getElementById('payButton');
-  
-  // Add a click event listener to the pay button
-  payButton.addEventListener('click', async () => {
-    // Call the payNow function when the button is clicked
-    await initializePayment();
-  });
+  submitButton.textContent = "Successfull";
+  submitButton.style.opacity = "0.5";
+  submitButton.disabled = true;
+};
